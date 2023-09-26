bc78774e0a05b5c636e0b90844d0699f2d3a54ba
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-09-27 00:23:29 +0600

    added no found text when not donated yet

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -19,25 +19,36 @@ const Donation = () => {
             setShowBtn(true);
       }
 
+      console.log(donatedCards);
+
       return (
             <div>
                   {
-                        showAll ? <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
-                              {
-                                    donatedCards.map(card => <DonatedCardDetails key={card.id} card={card}></DonatedCardDetails>)
-                              }
-                        </div> :
-                              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+                        donatedCards.length === 0 ? <div className="mt-10">
+                              <h1 className="text-4xl font-medium text-center my-7">You haven't donated anywhere yet!!</h1>
+                              <img className="w-96 mx-auto" src="https://i.ibb.co/JKwYvBN/49e58d5922019b8ec4642a2e2b9291c2.png" alt="" />
+                        </div>
+                              :
+                              <div>
                                     {
-                                          donatedCards.slice(0, 4).map(card => <DonatedCardDetails key={card.id} card={card}></DonatedCardDetails>)
+                                          showAll ? <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+                                                {
+                                                      donatedCards.map(card => <DonatedCardDetails key={card.id} card={card}></DonatedCardDetails>)
+                                                }
+                                          </div> :
+                                                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+                                                      {
+                                                            donatedCards.slice(0, 4).map(card => <DonatedCardDetails key={card.id} card={card}></DonatedCardDetails>)
+                                                      }
+                                                </div>
+                                    }
+
+                                    {
+                                          donatedCards.length >= 4 && <div className={`flex items-center justify-center ${showBtn && 'hidden'}`}>
+                                                <button onClick={handleShowAllBtn} className="btn bg-[#009444] hover:bg-green-700 text-[#FFF] font-semibold my-9">Show More</button>
+                                          </div>
                                     }
                               </div>
-                  }
-
-                  {
-                        donatedCards.length >= 4 && <div className={`flex items-center justify-center ${showBtn && 'hidden'}`}>
-                              <button onClick={handleShowAllBtn} className="btn bg-[#009444] hover:bg-green-700 text-[#FFF] font-semibold my-9">Show More</button>
-                        </div>
                   }
             </div>
       );
